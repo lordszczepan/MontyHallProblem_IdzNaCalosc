f30ed7e0ba667e87c18038a52ac5a26645e55afc
Lukasz Szczepka <l.szczepka@rescop.com>
2022-07-27 19:18:24 +0200

    Add project files.

---
+++ MontyHallProblem_IdzNaCalosc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontyHallProblem_IdzNaCalosc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
